43b9ac0472f8eb8dfc4d079378ef76d7aedb8793
mi_dehghan <mi_dehghan@isc.co.ir>
2024-11-27 08:44:49 +0330

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles.css";
+
+import Camera from "./Camera";
+import AllCameras from "./AllCamera";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Camera />
+      {/* <AllCameras /> */}
+    </div>
+  );
+}
